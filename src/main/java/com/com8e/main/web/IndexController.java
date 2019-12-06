7984006f6ec964cd96ba8ae207253f06f1d78e42
choi <Administrator@MSDN>
2019-12-06 22:31:15 +0900

    IndexController 수정, ojdbc 라이브러리 추가 19/12/06 22:31 최명환

---
+++ src/main/java/com/com8e/main/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.com8e.main.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping(value = "/")
+	public String home() {
+		return "index";
+	}
+	
+}
