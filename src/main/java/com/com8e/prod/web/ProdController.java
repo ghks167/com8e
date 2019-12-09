ad34d2fe1f06e23d7a967e2bd23ffe478fd2183c
pc16 <pc16@pc16-PC>
2019-12-09 17:02:48 +0900

    12/09 17:02 prdo VO, prod.xml 등등 조용혁

--- src/main/java/com/com8e/prod/web/ProdController.java
+++ src/main/java/com/com8e/prod/web/ProdController.java
@@ -1,20 +1,23 @@
 package com.com8e.prod.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.com8e.prod.vo.ProdVO;
 
 
 @Controller
 public class ProdController {
 	
 	@RequestMapping(value = "/prod/prodView")
-	public String ProdView() {
+	public String ProdView(@ModelAttribute("prod") ProdVO prod) {
 		String view ="prod/prodView";
 		return view;
 	}
 	
 	@RequestMapping(value = "/prod/prodForm")
-	public String ProdRegist() {
+	public String ProdRegist(@ModelAttribute("prod") ProdVO prod) {
 		String view ="prod/prodForm";
 		return view;
 	}
