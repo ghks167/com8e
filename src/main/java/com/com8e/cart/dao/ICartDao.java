91778ffa6d1cd70c7961a38f18ed79d972aa6937
choi <ghks167@naver.com>
2019-12-18 12:47:19 +0900

    cart 초기 !! 19/12/18 12:47 최명환

---
+++ src/main/java/com/com8e/cart/dao/ICartDao.java
@@ -0,0 +1,14 @@
+package com.com8e.cart.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.com8e.cart.vo.CartVO;
+
+@Mapper
+public interface ICartDao {
+	
+	public List<CartVO> selectCartList(String cart_mem) throws Exception;
+	
+}
